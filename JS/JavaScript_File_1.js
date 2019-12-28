53a6372b4c86d8b6b147f8d944237d55e94229c9
joeguernaccini <joegee831@gmail.com>
2019-12-28 18:31:51 -0500

    Step 58 of CSS Course

---
+++ JS/JavaScript_File_1.js
@@ -0,0 +1,27 @@
+var blues = "I have the blues.<br>"
+var blues = blues.fontcolor("blue")
+document.write(blues);
+
+var a = " I have a headache today.<br>"
+    + " My stomache is queezy<br>";
+var a = a.fontcolor("purple");
+document.write(a);
+
+var family = "The Guernaccini\'s", Dad = "Joe", Mom = "Ashly", Daughter = "Fiona";
+var family = family.fontcolor("red");
+document.write(Daughter);
+
+var green = "I like green<br>"
+var green = green.fontcolor("green");
+document.write(green);
+
+var work = "Dundermifflin", Boss = "Michael<br>";
+var work = work.fontcolor("brown");
+var Boss = Boss.fontcolor("brwon");
+document.write(Boss);
+document.write("\n \n")
+
+var result = (5 * 2);
+document.write(result);
+"\n"
+document.write(blues);
